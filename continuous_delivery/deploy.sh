0a2785a20e58a20c848b21acaa09471b3f869f8c
renzon <renzo.n@gmail.com>
2013-09-04 16:59:36 -0300

    8 try to accomplish #5 fixing app and version

--- continuous_delivery/deploy.sh
+++ continuous_delivery/deploy.sh
@@ -2,6 +2,6 @@
 
 continuous_delivery/build.sh
 
-$GAE_SDK/appcfg.py update build --email=<$BOT_EMAIL --passin=$BOT_PASSWORD
+$GAE_SDK/appcfg.py update build --email=$BOT_EMAIL --passin <<$BOT_PASSWORD
 
-$GAE_SDK/appcfg.py set_default_version build --email=<$BOT_EMAIL --passin=$BOT_PASSWORD
+$GAE_SDK/appcfg.py set_default_version build --email=$BOT_EMAIL --passin <<$BOT_PASSWORD
