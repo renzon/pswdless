5cbe9771721dca728c21de563d901220118b224d
renzon <renzo.n@gmail.com>
2013-09-04 15:40:11 -0300

    second try to accomplish #5 setting the SDK dir

--- continuous_delivery/deploy.sh
+++ continuous_delivery/deploy.sh
@@ -2,6 +2,6 @@
 
 continuous_delivery/build.sh
 
-appcfg.py update build --email=renzon@gmail.com --oauth2
+$GAE_SDK/appcfg.py update build --email=renzon@gmail.com --oauth2
 
-appcfg.py set_default_version build --email=renzon@gmail.com --oauth2
+$GAE_SDK/appcfg.py set_default_version build --email=renzon@gmail.com --oauth2
