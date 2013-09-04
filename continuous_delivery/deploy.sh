24bf035ed4f67fffcc51f6d799628efaa58207e9
renzon <renzo.n@gmail.com>
2013-09-04 15:56:42 -0300

    forth try to accomplish #5 fixing app and version

--- continuous_delivery/deploy.sh
+++ continuous_delivery/deploy.sh
@@ -2,6 +2,6 @@
 
 continuous_delivery/build.sh
 
-$GAE_SDK/appcfg.py update build --email=renzon@gmail.com --oauth2
+$GAE_SDK/appcfg.py update build --oauth2 --noauth_local_webserver
 
-$GAE_SDK/appcfg.py set_default_version build --email=renzon@gmail.com --oauth2
+$GAE_SDK/appcfg.py set_default_version build --email=renzon@gmail.com --oauth2 --noauth_local_webserver
