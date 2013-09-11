5f8d3a2a84afbf416b2e6ee87f1a5e606d7195c4
renzon <renzo.n@gmail.com>
2013-09-10 22:08:36 -0300

    close #10

--- src/web/setup.py
+++ src/web/setup.py
@@ -6,15 +6,16 @@ from zen import router
 
 
 def index(_resp,_handler):
-    setup_cmd=facade.initial_setup()
-    setup_cmd.execute()
-    if setup_cmd.result:
-        _resp.write('Initial setup ok')
-    else:
-        google_user=users.get_current_user()
-        if google_user:
-            _resp.write(setup_cmd.errors)
-        else:
-            return_url=router.to_path(index)
-            login_url=users.create_login_url(return_url)
-            _handler.redirect(login_url)
+    _resp.write('Setup disabled. You can enable by removing the coments on web.setup.py')
+    # setup_cmd=facade.initial_setup()
+    # setup_cmd.execute()
+    # if setup_cmd.result:
+    #     _resp.write('Initial setup ok')
+    # else:
+    #     google_user=users.get_current_user()
+    #     if google_user:
+    #         _resp.write(setup_cmd.errors)
+    #     else:
+    #         return_url=router.to_path(index)
+    #         login_url=users.create_login_url(return_url)
+    #         _handler.redirect(login_url)
