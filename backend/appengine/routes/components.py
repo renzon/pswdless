ef2e383b5b1a477c5238d9b904d0daf3251fcb33
renzon <renzon@gmail.com>
2015-01-05 19:10:40 -0200

    /componentes Ok

--- backend/appengine/routes/components.py
+++ backend/appengine/routes/components.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from config.template_middleware import TemplateResponse
+from gaecookie.decorator import no_csrf
+from gaepermission.decorator import login_not_required
 
 
-def index(_write_tmpl):
-    _write_tmpl("templates/sites_mock.html")
+@login_not_required
+@no_csrf
+def index():
+    return TemplateResponse(template_path="sites_mock.html")
 
